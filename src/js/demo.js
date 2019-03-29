a005581be4b908a952902e01f0abd8f7e6954bfb
Alex Hoyau <a.hoyau@silexlabs.org>
2019-03-28 20:13:51 -0600

    comments in the sample

--- src/js/demo.js
+++ src/js/demo.js
@@ -1,8 +1,14 @@
 import {Stage} from "./Stage";
+import { cpus } from "os";
 
 window.onload = function() {
-  document.querySelector('#iframe').contentDocument.write(document.querySelector('#random-content').innerHTML);
-  var stage = new Stage(document.querySelector('#iframe'));
+  // find the empty iframe in the page
+  const iframe = document.querySelector('#iframe');
+  // write some content in the iframe
+  iframe.contentDocument.write(document.querySelector('#random-content').innerHTML);
+  // create the Stage class
+  const stage = new Stage(iframe);
+  // listen to the events and print in the console
   stage.on('drop', (elements) => {
     console.log(`${ elements.length } elements have been dropped to ${ elements[0].parentElement }`)
   });
