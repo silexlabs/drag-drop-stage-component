b389ec341b9362d6c190a8e74f179535781bd5b8
Alex Hoyau <a.hoyau@silexlabs.org>
2019-04-13 18:47:30 -0500

    re-activate unit tests

--- tests/handlers/movehandler.test.electron.ts
+++ tests/handlers/movehandler.test.electron.ts
@@ -65,140 +65,140 @@ describe('MoveHandler', function() {
     jest.spyOn(stageStoreMock, 'getState');
   });
 
-  // it('should move an absolute element in the dom', function() {
-  //   // init
-  //   stageStoreMock.selectableElem1.selected = true;
-  //   handler = initHandler();
-  //   expect(handler.selection.length).toBe(1);
-  //   expect(handler.selection[0].el).toBe(StageStoreMock.elem1);
-  //   expect(handler.selection[0].metrics.clientRect.top).toBe(100);
-  //   expect(stageStoreMock.subscribe).toBeCalledTimes(1);
+  it('should move an absolute element in the dom', function() {
+    // init
+    stageStoreMock.selectableElem1.selected = true;
+    handler = initHandler();
+    expect(handler.selection.length).toBe(1);
+    expect(handler.selection[0].el).toBe(StageStoreMock.elem1);
+    expect(handler.selection[0].metrics.clientRect.top).toBe(100);
+    expect(stageStoreMock.subscribe).toBeCalledTimes(1);
 
-  //   // test
-  //   var mouseData = {
-  //     movementX: 0,
-  //     movementY: 100,
-  //     mouseX: 0,
-  //     mouseY: 100,
-  //     shiftKey: false,
-  //     target: StageStoreMock.elem1,
-  //   };
-  //   stageStoreMock.mouseState.mouseData = mouseData;
-  //   handler.update(mouseData);
-  //   expect(stageStoreMock.dispatch).toBeCalledTimes(2);
-  //   var calls = stageStoreMock.dispatch['mock'].calls;
-  //   var lastAction = calls[calls.length - 1][0];
-  //   expect(lastAction.type).toBe('SELECTABLE_UPDATE');
-  //   expect(lastAction.selectables.length).toBe(1);
-  //   expect(lastAction.selectables[0].el).toBe(StageStoreMock.elem1);
-  //   expect(lastAction.selectables[0].metrics.clientRect.top).toBe(200);
-  // });
+    // test
+    var mouseData = {
+      movementX: 0,
+      movementY: 100,
+      mouseX: 0,
+      mouseY: 100,
+      shiftKey: false,
+      target: StageStoreMock.elem1,
+    };
+    stageStoreMock.mouseState.mouseData = mouseData;
+    handler.update(mouseData);
+    expect(stageStoreMock.dispatch).toBeCalledTimes(2);
+    var calls = stageStoreMock.dispatch['mock'].calls;
+    var lastAction = calls[calls.length - 1][0];
+    expect(lastAction.type).toBe('SELECTABLE_UPDATE');
+    expect(lastAction.selectables.length).toBe(1);
+    expect(lastAction.selectables[0].el).toBe(StageStoreMock.elem1);
+    expect(lastAction.selectables[0].metrics.clientRect.top).toBe(200);
+  });
 
-  // it('should move an element in the flow', function() {
-  //   // init
-  //   stageStoreMock.selectableElem2.selected = true;
-  //   stageStoreMock.selectableElem2.metrics.position = 'static';
-  //   handler = initHandler();
+  it('should move an element in the flow', function() {
+    // init
+    stageStoreMock.selectableElem2.selected = true;
+    stageStoreMock.selectableElem2.metrics.position = 'static';
+    handler = initHandler();
 
-  //   // test
-  //   var mouseData = {
-  //     movementY: 150 - 15, // from middle of elem2
-  //     movementX: 150 - 15, // from middle of elem2
-  //     mouseX: 150, // to middle of elem1
-  //     mouseY: 150, // to middle of elem1
-  //     shiftKey: false,
-  //     target: StageStoreMock.elem2,
-  //   };
-  //   stageStoreMock.mouseState.mouseData = mouseData;
-  //   handler.update(mouseData);
-  //   handler.release();
-  //   expect(stageStoreMock.dispatch).toBeCalledTimes(4);
-  //   var calls = stageStoreMock.dispatch['mock'].calls;
-  //   var lastAction = calls[calls.length - 1][0];
-  //   expect(lastAction.type).toBe('SELECTABLE_UPDATE');
-  //   expect(lastAction.selectables.length).toBe(2);
-  //   expect(lastAction.selectables[1].el).toBe(StageStoreMock.elem2);
-  //   expect(lastAction.selectables[1].el.parentElement).toBe(StageStoreMock.elem1);
-  //   expect(lastAction.selectables[1].metrics.clientRect.top).toBe(110);
-  //   expect(StageStoreMock.elem2.style.transform).toBe('');
-  // });
+    // test
+    var mouseData = {
+      movementY: 150 - 15, // from middle of elem2
+      movementX: 150 - 15, // from middle of elem2
+      mouseX: 150, // to middle of elem1
+      mouseY: 150, // to middle of elem1
+      shiftKey: false,
+      target: StageStoreMock.elem2,
+    };
+    stageStoreMock.mouseState.mouseData = mouseData;
+    handler.update(mouseData);
+    handler.release();
+    expect(stageStoreMock.dispatch).toBeCalledTimes(4);
+    var calls = stageStoreMock.dispatch['mock'].calls;
+    var lastAction = calls[calls.length - 1][0];
+    expect(lastAction.type).toBe('SELECTABLE_UPDATE');
+    expect(lastAction.selectables.length).toBe(2);
+    expect(lastAction.selectables[1].el).toBe(StageStoreMock.elem2);
+    expect(lastAction.selectables[1].el.parentElement).toBe(StageStoreMock.elem1);
+    expect(lastAction.selectables[1].metrics.clientRect.top).toBe(110);
+    expect(StageStoreMock.elem2.style.transform).toBe('');
+  });
 
-  // it('should find 1 droppable at (150, 150) while dragging elem1', function() {
-  //   // init
-  //   stageStoreMock.selectableElem2.selected = true;
-  //   stageStoreMock.selectableElem2.metrics.position = 'static';
-  //   handler = initHandler();
+  it('should find 1 droppable at (150, 150) while dragging elem1', function() {
+    // init
+    stageStoreMock.selectableElem2.selected = true;
+    stageStoreMock.selectableElem2.metrics.position = 'static';
+    handler = initHandler();
 
-  //   // test
-  //   var droppables = handler.findDropZonesUnderMouse(150, 150);
-  //   expect(droppables instanceof Array).toBe(true);
-  //   expect(droppables.length).toBe(2);
-  //   expect(droppables[0]).toBe(StageStoreMock.elem1);
-  // });
+    // test
+    var droppables = handler.findDropZonesUnderMouse(150, 150);
+    expect(droppables instanceof Array).toBe(true);
+    expect(droppables.length).toBe(2);
+    expect(droppables[0]).toBe(StageStoreMock.elem1);
+  });
 
-  // it('move to a new container, keep the position', function() {
-  //   // init
-  //   stageStoreMock.selectableElem1.selected = false;
-  //   stageStoreMock.selectableElem1.isDropZone = true;
-  //   stageStoreMock.selectableElem2.selected = true;
-  //   stageStoreMock.selectableElem2.metrics.position = 'absolute';
-  //   handler = initHandler();
-  //   expect(handler.selection.length).toBe(1);
-  //   expect(handler.selection[0].el).toBe(StageStoreMock.elem2);
-  //   expect(handler.selection[0].metrics.clientRect.top).toBe(10);
-  //   expect(handler.selection[0].metrics.position).toBe('absolute');
-  //   expect(stageStoreMock.subscribe).toBeCalledTimes(1);
+  it('move to a new container, keep the position', function() {
+    // init
+    stageStoreMock.selectableElem1.selected = false;
+    stageStoreMock.selectableElem1.isDropZone = true;
+    stageStoreMock.selectableElem2.selected = true;
+    stageStoreMock.selectableElem2.metrics.position = 'absolute';
+    handler = initHandler();
+    expect(handler.selection.length).toBe(1);
+    expect(handler.selection[0].el).toBe(StageStoreMock.elem2);
+    expect(handler.selection[0].metrics.clientRect.top).toBe(10);
+    expect(handler.selection[0].metrics.position).toBe('absolute');
+    expect(stageStoreMock.subscribe).toBeCalledTimes(1);
 
-  //   // test
-  //   var mouseData = {
-  //     movementY: 150 - 15, // from middle of elem2
-  //     movementX: 150 - 15, // from middle of elem2
-  //     mouseX: 150, // to middle of elem1
-  //     mouseY: 150, // to middle of elem1
-  //     shiftKey: false,
-  //     target: StageStoreMock.elem2,
-  //   };
-  //   stageStoreMock.mouseState.mouseData = mouseData;
-  //   handler.update(mouseData);
-  //   handler.release();
-  //   expect(stageStoreMock.dispatch).toBeCalledTimes(4);
+    // test
+    var mouseData = {
+      movementY: 150 - 15, // from middle of elem2
+      movementX: 150 - 15, // from middle of elem2
+      mouseX: 150, // to middle of elem1
+      mouseY: 150, // to middle of elem1
+      shiftKey: false,
+      target: StageStoreMock.elem2,
+    };
+    stageStoreMock.mouseState.mouseData = mouseData;
+    handler.update(mouseData);
+    handler.release();
+    expect(stageStoreMock.dispatch).toBeCalledTimes(4);
 
-  //   // check the actual position of the target
-  //   // and move it to match the provided absolute position
-  //   var calls = stageStoreMock.dispatch['mock'].calls;
-  //   var lastAction = calls[calls.length - 2][0];
-  //   expect(lastAction.type).toBe('SELECTABLE_UPDATE');
-  //   expect(lastAction.selectables.length).toBe(1);
-  //   expect(lastAction.selectables[0].el).toBe(StageStoreMock.elem2);
-  //   expect(lastAction.selectables[0].metrics.clientRect.top).toBe(145);
-  //   expect(lastAction.selectables[0].metrics.computedStyleRect.top).toBe(145 - 100);
-  // });
+    // check the actual position of the target
+    // and move it to match the provided absolute position
+    var calls = stageStoreMock.dispatch['mock'].calls;
+    var lastAction = calls[calls.length - 2][0];
+    expect(lastAction.type).toBe('SELECTABLE_UPDATE');
+    expect(lastAction.selectables.length).toBe(1);
+    expect(lastAction.selectables[0].el).toBe(StageStoreMock.elem2);
+    expect(lastAction.selectables[0].metrics.clientRect.top).toBe(145);
+    expect(lastAction.selectables[0].metrics.computedStyleRect.top).toBe(145 - 100);
+  });
 
-  // it('should rebuild the whole store metrics after drop', function() {
-  //   // init
-  //   stageStoreMock.selectableElem2.selected = true;
-  //   stageStoreMock.selectableElem2.metrics.position = 'static';
-  //   handler = initHandler();
+  it('should rebuild the whole store metrics after drop', function() {
+    // init
+    stageStoreMock.selectableElem2.selected = true;
+    stageStoreMock.selectableElem2.metrics.position = 'static';
+    handler = initHandler();
 
-  //   // test
-  //   var mouseData = {
-  //     movementY: 1, // from middle of elem2
-  //     movementX: 1, // from middle of elem2
-  //     mouseX: 16, // to middle of nowhere
-  //     mouseY: 16, // to middle of nowhere
-  //     shiftKey: false,
-  //     target: StageStoreMock.elem2,
-  //   };
-  //   stageStoreMock.mouseState.mouseData = mouseData;
-  //   handler.update(mouseData);
-  //   handler.release();
-  //   expect(stageStoreMock.dispatch).toBeCalledTimes(4);
-  //   var calls = stageStoreMock.dispatch['mock'].calls;
-  //   var lastAction = calls[calls.length - 1][0];
-  //   expect(lastAction.type).toBe('SELECTABLE_UPDATE');
-  //   expect(lastAction.selectables.length).toBe(2);
-  //   expect(lastAction.selectables[0].el.parentElement.id).toBe('container1');
-  // });
+    // test
+    var mouseData = {
+      movementY: 1, // from middle of elem2
+      movementX: 1, // from middle of elem2
+      mouseX: 16, // to middle of nowhere
+      mouseY: 16, // to middle of nowhere
+      shiftKey: false,
+      target: StageStoreMock.elem2,
+    };
+    stageStoreMock.mouseState.mouseData = mouseData;
+    handler.update(mouseData);
+    handler.release();
+    expect(stageStoreMock.dispatch).toBeCalledTimes(4);
+    var calls = stageStoreMock.dispatch['mock'].calls;
+    var lastAction = calls[calls.length - 1][0];
+    expect(lastAction.type).toBe('SELECTABLE_UPDATE');
+    expect(lastAction.selectables.length).toBe(2);
+    expect(lastAction.selectables[0].el.parentElement.id).toBe('container1');
+  });
 
   it('should not move elements and their parent', function() {
     StageStoreMock.additionalSelectables.push({
