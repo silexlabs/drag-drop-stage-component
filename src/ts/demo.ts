fc816d4a3b925d8c1f6002330cd7eb133cde931a
Alex Hoyau <a.hoyau@silexlabs.org>
2019-04-15 18:45:25 -0500

    added favicon and removed console.log

--- src/ts/demo.ts
+++ src/ts/demo.ts
@@ -10,27 +10,27 @@ new Stage(iframe, iframe.contentDocument.querySelectorAll('.stage-element'), {
   onDrop: (selectables) => {
     const dropElement = selectables[0].dropZone.parent;
     const str = `${ selectables.length } elements have been droped to ${ dropElement.tagName.toLocaleLowerCase() }${ dropElement.id ? '#' + dropElement.id : '' }${ Array.from(dropElement.classList).map(c => '.' + c).join('') }`;
-    console.log(str);
+    // console.log(str);
     output.value = str;
   },
   onDrag: (selectables, boundingBox) => {
     const str = `${ selectables.length } elements are being draged`;
-    console.log(str);
+    // console.log(str);
     output.value = str;
   },
   onResize: (selectables, boundingBox) => {
     const str = `${ selectables.length } elements have been resizeed to ${JSON.stringify(boundingBox)}`;
-    console.log(str);
+    // console.log(str);
     output.value = str;
   },
   onDraw: (selectables, boundingBox) => {
     const str = `Drawing region: ${JSON.stringify(boundingBox)}`;
-    console.log(str);
+    // console.log(str);
     output.value = str;
   },
   onSelect: (selectables) => {
     const str = `${ selectables.length } elements have been (un)selected`;
-    console.log(str);
+    // console.log(str);
     output.value = str;
   },
 });
